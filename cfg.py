07a4f3db457564c591d9048a2d407b45f2273296
TYM-laptop <tym1060326@163.com>
2015-01-20 10:57:53 +0800

    add abstract into bib so that user can preview the content without downloading pdfs

--- cfg.py
+++ cfg.py
@@ -1,37 +1,72 @@
 import os
 import logging
+import logging.handlers
 from multiprocessing.dummy import Pool as ThreadPool
-
-
-log = logging.Logger('command', level=logging.DEBUG)
 
 # TODO: find all formats
 FORMAT = 'bibtex'
 
 URL_ROOT = 'http://dl.acm.org/'
+URL_CITATION = URL_ROOT + 'citation.cfm?id={id}&preflayout=flat'
 URL_BIBTEX = URL_ROOT + 'exportformats.cfm?id={id}&expformat=' + FORMAT
 
 INVALID_CHARS = list('/?|:*\"<>\\')
 
-CONFERENCE_PAGE = './pages/Proceedings of the 37th international ACM SIGIR conference on Research & development in information retrieval.html'
+CONFERENCE_PAGE = []
+with open('pages.txt') as f:
+    for l in f:
+        if l.strip():
+            CONFERENCE_PAGE += [l.strip()]
 
 BIB_FILE = ''
 BIB_ENCODING = 'utf8'
 
-SLEEP_TIME_RANGE = (4, 10)
-RETRY = 1
-WORKER = 35
 
-# log.info('loading proxy file...')
-print('loading proxy file...')
+
+# CONFERENCE_NAME = os.path.basename(CONFERENCE_PAGE)
+# name = CONFERENCE_NAME.split('.')
+# name[-1] = 'bib'
+# CONFERENCE_BIB = '.'.join(name)
+# name[-1] = 'log'
+# CONFERENCE_LOG = '.'.join(name)
+CONFERENCE_LOG = '2015-01-17.log'
+
+log = logging.getLogger('my logger')
+
+handler = logging.handlers.RotatingFileHandler(CONFERENCE_LOG)
+fmt = '%(asctime)s - %(message)s'
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+handler = logging.StreamHandler()
+fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+log.setLevel(logging.DEBUG)
+
+log.info('loading proxy file...')
 PROXIES = []
+PROXY_TABOO = []
 with open('proxy.txt') as f:
     PROXIES = [l for l in f]
-print('%d proxies loaded.' % len(PROXIES))
-# log.info('%d proxies loaded.' % len(PROXIES))
+log.info('%d proxies loaded.' % len(PROXIES))
 
-print('testing proxies...')
-# log.info('testing proxies...')
+SLEEP_TIME_RANGE = (30, 150) # (2, 5)
+
+# the distribution shape parameters of random sleep time
+SLEEP_ALPHA = 2
+SLEEP_BETA = 5
+
+RETRY = 3
+WORKER = int(len(PROXIES) * 0.3)
+if WORKER > 15:
+    WORKER = 15
+
+log.info('%d workers boosted' % WORKER)
+log.info('testing proxies...')
 
 pool = ThreadPool(WORKER)
 def is_available(ip):
@@ -45,11 +80,7 @@ pool.join()
 
 PROXIES = list(filter(lambda ip: ip, PROXIES))
 if PROXIES:
-    print('%d proxies available.' % len(PROXIES))
-    # log.info('%d proxies available.' % len(PROXIES))
+    log.info('%d proxies available.' % len(PROXIES))
 else:
-    print('no proxy available.')
-    # log.warning('no proxy available.')
+    log.warning('no proxy available.')
 
-
-ERROR_OUTPUT = 'err.txt'
--- crawler.py
+++ crawler.py
@@ -2,36 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from urllib import request, parse, error
-# import requests
+import requests
 import os
 import time
 import random
-import logging
+import http
 from multiprocessing.dummy import Pool as ThreadPool
 from bs4 import BeautifulSoup
+import bibtexparser as bp
 import cfg
 
 random.seed(time.time())
 log = cfg.log
 
-def main():
-    bibs = get_url_citations(cfg.CONFERENCE_PAGE)
-    # bibs, errs = get_url_citations(cfg.CONFERENCE_PAGE)
-    conference_name = os.path.basename(cfg.CONFERENCE_PAGE)
-    name = conference_name.split('.')
-    name[-1] = 'bib'
-    conference_name = '.'.join(name)
 
-    bib_file = cfg.BIB_FILE if cfg.BIB_FILE else get_valid_file_name(conference_name)
-    with open(bib_file, 'w', encoding=cfg.BIB_ENCODING) as f:
-        for bib in bibs:
-            f.write(bibs[bib])
-            f.write('\n')
-        f.write(get_group_tree_string(bibs))
-    # with open(cfg.ERROR_OUTPUT, 'w', encoding=cfg.BIB_ENCODING) as f:
-    #     for err in errs:
-    #         f.write(err)
-    #         f.write('\n')
+def main():
+    for conference_page in cfg.CONFERENCE_PAGE:
+        bibs = get_url_citations(conference_page)
+        # bibs, errs = get_url_citations(conference_page)
+        conference_name = os.path.basename(conference_page)
+        name = conference_name.split('.')
+        name[-1] = 'bib'
+        conference_name = '.'.join(name)
+
+        bib_file = cfg.BIB_FILE if cfg.BIB_FILE else get_valid_file_name(conference_name)
+        with open(bib_file, 'w', encoding=cfg.BIB_ENCODING) as f:
+            for bib in bibs:
+                f.write(bibs[bib])
+                f.write('\n')
+            f.write(get_group_tree_string(bibs, conference_page))
+            # with open(cfg.ERROR_OUTPUT, 'w', encoding=cfg.BIB_ENCODING) as f:
+            # for err in errs:
+            #         f.write(err)
+            #         f.write('\n')
 
 
 def retry(func):
@@ -39,42 +42,60 @@ def retry(func):
         for i in range(1 + cfg.RETRY):
             try:
                 return func(url)
-            except (error.HTTPError, error.URLError, ConnectionResetError) as e:
-                # logging.error(e)
-                # logging.info('retrying...')
-                print('error occurs, retrying...')
+            # except (error.HTTPError, error.URLError,
+            # ConnectionResetError, http.client.BadStatusLine) as e:
+            except Exception as e:
+                log.error(e)
+                log.info('%s error occurs, retrying...' % url)
                 time.sleep(10)
+
     return _decorator
 
 
-def random_proxy(func):
-    def set_proxy(proxy):
-        proxy_handler = request.ProxyHandler({'http': proxy})
-        opener = request.build_opener(proxy_handler)
-        request.install_opener(opener)
-    def _decorator(url):
-        if len(cfg.PROXIES) > 0:
-            r_index = random.randint(0, len(cfg.PROXIES)-1)
-            set_proxy(cfg.PROXIES[r_index])
-        return func(url)
-    return _decorator
+# def random_proxy(func):
+#     def set_proxy(proxy):
+#         proxy_handler = request.ProxyHandler({'http': proxy})
+#         opener = request.build_opener(proxy_handler)
+#         request.install_opener(opener)
+
+#     def _decorator(url):
+#         if len(cfg.PROXIES) > 0:
+#             r_index = random.randint(0, len(cfg.PROXIES) - 1)
+#             set_proxy(cfg.PROXIES[r_index])
+#         return func(url)
+
+#     return _decorator
 
 
 @retry
-@random_proxy
 def urlopen(url):
+    # random_proxy
+    proxy = None
+    if len(cfg.PROXIES) > 0:
+        r_index = random.randint(0, len(cfg.PROXIES) - 1)
+        while cfg.PROXIES[r_index] in cfg.PROXY_TABOO:
+            r_index = random.randint(0, len(cfg.PROXIES) - 1)
+        proxy = {'http': cfg.PROXIES[r_index]}
     # without this camouflage, response will return HTTP Error 403 Forbidden
     headers = {
-        'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
     }
+    if proxy:
+        proxy_handler = request.ProxyHandler(proxy)
+        opener = request.build_opener(proxy_handler)
+        request.install_opener(opener)
     req = request.Request(url, headers=headers)
     res = request.urlopen(req)
+    if res.getcode() != 200 and proxy:
+        if proxy['http'] not in cfg.PROXY_TABOO:
+            cfg.PROXY_TABOO += [proxy['http']]
+            log.info('%d available proxies'
+                % (len(cfg.PROXIES) - len(cfg.PROXY_TABOO)))
     return res
 
 
 def get_citation_id(func):
     def _decorator(url_citation):
-        # url_citation = 'citation.cfm?id=2617557&CFID=586594267&CFTOKEN=76400862'
         url_citation_parsed = parse.urlparse(url_citation)
         params = parse.parse_qs(url_citation_parsed.query)
 
@@ -83,27 +104,54 @@ def get_citation_id(func):
         else:
             # TODO: throw exception?
             pass
+
     return _decorator
 
 
 def random_sleep(func):
     def _decorator(citation_id):
+        t = random.betavariate(cfg.SLEEP_ALPHA, cfg.SLEEP_BETA) \
+                * (cfg.SLEEP_TIME_RANGE[1] - cfg.SLEEP_TIME_RANGE[0]) + cfg.SLEEP_TIME_RANGE[0]
+        time.sleep(t)
         text = func(citation_id)
-        time.sleep(random.uniform(cfg.SLEEP_TIME_RANGE[0], cfg.SLEEP_TIME_RANGE[1]))
+        # time.sleep(random.uniform(cfg.SLEEP_TIME_RANGE[0], cfg.SLEEP_TIME_RANGE[1]))
         return text
+
     return _decorator
 
 
 @random_sleep
 @get_citation_id
 def get_citation_text(citation_id):
-    print(citation_id)
+    log.info(citation_id)
     res = urlopen(cfg.URL_BIBTEX.format(id=citation_id))
     if res:
         soup = BeautifulSoup(res.read())
         if soup.pre:
-            return {citation_id: soup.pre.get_text()}
+            bibtex = soup.pre.get_text()
+            bib = bp.loads(bibtex)
+            bib.entries[0]['abstract'] = get_abstract(citation_id)
+            return {citation_id: bp.dumps(bib)}
     return ''
+
+
+def get_abstract(citation_id):
+    abstract = ''
+    res = requests.get(cfg.URL_CITATION.format(id=citation_id))
+    # res = requests.get('http://dl.acm.org/citation.cfm?doid=1277741.1277774&preflayout=flat')
+    log.info(citation_id + ' status - ' + str(res.status_code))
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text)
+        if soup.find('div', {'class': 'flatbody'}):
+            log.info(citation_id + ' - abstract not found')
+            if soup.find('div', {'class': 'flatbody'}).find('div'):
+                abstract = soup.find('div', {'class': 'flatbody'}).find('div').text
+                if '<p>' in abstract:
+                    abstract = abstract.replace('<p>', '').replace('</p>', '\n')
+                else:
+                    abstract
+                log.info('fetched abstract - ' + citation_id)
+    return abstract
 
 
 def get_url_citations(url_conference):
@@ -113,8 +161,8 @@ def get_url_citations(url_conference):
     # url_conference = 'http://dl.acm.org/citation.cfm?id=2600428&picked=prox&CFID=586594267&CFTOKEN=76400862'
     # res = urlopen(url_conference)
     # soup = BeautifulSoup(res.read())
-    citations = {}
-    error_urls = []
+    # citations = {}
+    # error_urls = []
 
     with open(url_conference, encoding='utf-8') as f:
         soup = BeautifulSoup(f)
@@ -123,8 +171,7 @@ def get_url_citations(url_conference):
         url_citations = filter(lambda url: 'citation.cfm' in url, urls)
         url_citations = list(url_citations)
 
-    print('%d citations found.' % len(url_citations))
-    # log.info('%d citations found.' % len(url_citations))
+    log.info('%d citations found.' % len(url_citations))
 
     pool = ThreadPool(cfg.WORKER)
     citations = pool.map(get_citation_text, url_citations)
@@ -138,7 +185,7 @@ def get_url_citations(url_conference):
                 temp_citations[key] = citation[key]
     citations = temp_citations
 
-    print('%d of %d citations fetched.' % (len(citations), len(url_citations)))
+    log.info('%d of %d citations fetched.' % (len(citations), len(url_citations)))
     return citations  # , error_urls
 
 
@@ -149,24 +196,24 @@ def get_valid_file_name(file_name):
 
 
 def get_jabref_comment_string(func):
-    def _decorator(bibs):
+    def _decorator(bibs, conference_page):
         return '@comment{jabref-meta: groupsversion:3;}\n' \
-                + '@comment{jabref-meta: groupstree:\n%s}' % func(bibs)
+               + '@comment{jabref-meta: groupstree:\n%s}' % func(bibs, conference_page)
+
     return _decorator
 
 
 @get_jabref_comment_string
-def get_group_tree_string(bibs):
+def get_group_tree_string(bibs, conference_page):
     def get_id_key(bibs):
         citation_id_key = {}
         for citation_id in bibs:
-            citation_id_key[citation_id] = bibs[citation_id].strip()  \
+            citation_id_key[citation_id] = bibs[citation_id].strip() \
                 .split('\n')[0].split('{')[-1].split(',')[0]
         return citation_id_key
 
-
     def get_groups(url_conference):
-        groups = {'':[],}
+        groups = {'': [], }
         with open(url_conference, encoding='utf-8') as f:
             soup = BeautifulSoup(f)
             current_session = ''
@@ -182,12 +229,12 @@ def get_group_tree_string(bibs):
                             groups[current_session] += [get_citation_id(lambda e: e)(url)]
         return groups
 
-    groups = get_groups(cfg.CONFERENCE_PAGE)
+    groups = get_groups(conference_page)
     id_key = get_id_key(bibs)
 
     def get_group_string(group):
-        group_keys = map(lambda citation_id: id_key[citation_id] \
-            if citation_id in id_key else '', groups[group])
+        group_keys = map(lambda citation_id: id_key[citation_id]
+                         if citation_id in id_key else '', groups[group])
         group_keys = filter(lambda e: e, group_keys)
         if group:
             return '1 ExplicitGroup:%s\\;0\\;%s\\;;' % (group, '\\;'.join(group_keys))
@@ -197,5 +244,5 @@ def get_group_tree_string(bibs):
     group_tree = map(get_group_string, groups.keys())
     return '\n'.join(group_tree)
 
-main()
 
+main()
