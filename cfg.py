0ae90bc26893ab0da12f33444265d3abe31567eb
tangym <yut44@cse.psu.edu>
2017-05-31 17:07:36 -0400

    code refactoring

--- cfg.py
+++ cfg.py
@@ -1,7 +1,5 @@
-import os
 import logging
 import logging.handlers
-from multiprocessing.dummy import Pool as ThreadPool
 
 # TODO: find all formats
 FORMAT = 'bibtex'
@@ -12,24 +10,10 @@ URL_BIBTEX = URL_ROOT + 'exportformats.cfm?id={id}&expformat=' + FORMAT
 
 INVALID_CHARS = list('/?|:*\"<>\\')
 
-CONFERENCE_PAGE = []
-with open('pages.txt') as f:
-    for l in f:
-        if l.strip():
-            CONFERENCE_PAGE += [l.strip()]
-
 BIB_FILE = ''
 BIB_ENCODING = 'utf8'
 
-
-
-# CONFERENCE_NAME = os.path.basename(CONFERENCE_PAGE)
-# name = CONFERENCE_NAME.split('.')
-# name[-1] = 'bib'
-# CONFERENCE_BIB = '.'.join(name)
-# name[-1] = 'log'
-# CONFERENCE_LOG = '.'.join(name)
-CONFERENCE_LOG = '2015-01-17.log'
+CONFERENCE_LOG = '2017-05-31.log'
 
 log = logging.getLogger('my logger')
 
@@ -47,40 +31,6 @@ log.addHandler(handler)
 
 log.setLevel(logging.DEBUG)
 
-log.info('loading proxy file...')
-PROXIES = []
-PROXY_TABOO = []
-with open('proxy.txt') as f:
-    PROXIES = [l for l in f]
-log.info('%d proxies loaded.' % len(PROXIES))
-
-SLEEP_TIME_RANGE = (30, 150) # (2, 5)
-
-# the distribution shape parameters of random sleep time
+SLEEP_TIME_RANGE = (0, 5)
 SLEEP_ALPHA = 2
 SLEEP_BETA = 5
-
-RETRY = 3
-WORKER = int(len(PROXIES) * 0.3)
-if WORKER > 15:
-    WORKER = 15
-
-log.info('%d workers boosted' % WORKER)
-log.info('testing proxies...')
-
-pool = ThreadPool(WORKER)
-def is_available(ip):
-    if os.system('ping %s -n 1 > nul' % ip.split(':')[1][2:]) == 0:
-        return ip
-    else:
-        return ''
-PROXIES = pool.map(is_available, PROXIES)
-pool.close()
-pool.join()
-
-PROXIES = list(filter(lambda ip: ip, PROXIES))
-if PROXIES:
-    log.info('%d proxies available.' % len(PROXIES))
-else:
-    log.warning('no proxy available.')
-
--- crawler.py
+++ crawler.py
@@ -3,6 +3,8 @@
 
 import platform
 import re
+import random
+import time
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -19,18 +21,17 @@ else:
 browser = webdriver.PhantomJS(PHANTOMJS_PATH)
 
 
-url = r'http://dl.acm.org/citation.cfm?id=2750858&preflayout=flat'
+def random_sleep(func):
+    def inner(*args, **kwargs):
+        t = random.betavariate(cfg.SLEEP_ALPHA, cfg.SLEEP_BETA) \
+            * (cfg.SLEEP_TIME_RANGE[1] - cfg.SLEEP_TIME_RANGE[0]) \
+            + cfg.SLEEP_TIME_RANGE[0]
+        time.sleep(t)
+        return func(*args, **kwargs)
+    return inner
 
 
-def extract_paper_ids(html):
-    soup = BeautifulSoup(html, "html.parser")
-    links = soup.findAll('a')
-    links = filter(lambda link: 'citation.cfm' in str(link), links)
-    ids = map(lambda link: re.findall(r'(?<=id=)\d+(?=.*)', str(link)), links)
-    ids = map(lambda e: e[0], filter(lambda e: e, ids))
-    return set(ids)
-
-
+@random_sleep
 def get(url, execute_js=True, proxy=''):
     if not execute_js:
         # without this camouflage, response will return 403 Forbidden
@@ -53,10 +54,25 @@ def get(url, execute_js=True, proxy=''):
         return browser.page_source
 
 
+# Confererence page parser
+def extract_paper_ids(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    links = soup.findAll('a')
+    links = filter(lambda link: 'citation.cfm' in str(link), links)
+    ids = map(lambda link: re.findall(r'(?<=id=)\d+(?=.*)', str(link)), links)
+    ids = map(lambda e: e[0], filter(lambda e: e, ids))
+    return set(ids)
+
+
+# Conference page parser
+def extract_conference_title(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    return soup.find('strong').text
+
+
 def export_citation(paper_id):
-    # log.info(citation_id)
-    html_content = get(cfg.URL_BIBTEX.format(id=paper_id))
-    soup = BeautifulSoup(html_content)
+    html_content = get(cfg.URL_BIBTEX.format(id=paper_id), execute_js=False)
+    soup = BeautifulSoup(html_content, 'html.parser')
     if soup.pre:
         bibtex = soup.pre.get_text()
         bib = bibtexparser.loads(bibtex)
@@ -67,8 +83,8 @@ def export_citation(paper_id):
 
 def get_abstract(paper_id):
     abstract = ''
-    html_content = get(cfg.URL_CITATION.format(id=paper_id))
-    soup = BeautifulSoup(html_content)
+    html_content = get(cfg.URL_CITATION.format(id=paper_id), execute_js=False)
+    soup = BeautifulSoup(html_content, 'html.parser')
     if soup.find('div', {'class': 'flatbody'}):
         if soup.find('div', {'class': 'flatbody'}).find('div'):
             abstract = soup.find('div', {'class': 'flatbody'}).find('div').text
